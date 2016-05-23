e06149b230f75f00781f10d81a8da69836c0f955
CaillPa <azertyu76@gmail.com>
2016-05-23 14:05:41 +0200

    cleaning

--- videosimple.py
+++ videosimple.py
@@ -1,3 +1,6 @@
+"""
+    Lecture et affichage de la video
+"""
 from pivideostream import PiVideoStream
 from imutils.video import FPS # Pour les mesures de framerate
 import cv2
@@ -6,11 +9,6 @@ import numpy as np
 
 # Nom de la fenetre d'affichage
 window_name = 'preview'
-
-# Taille du kernel pour le filtre Gaussien
-gaussSize = (3, 3)
-
-ddepth = cv2.CV_16S
 
 # Creation du thread de lecture + setup
 vs=PiVideoStream()
@@ -25,29 +23,11 @@ cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
 fps = FPS().start()
 
 while True :
+
     frame = vs.read()
-
-    # Passage en NdG + filtre Gaussien pour réduire le bruit
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, gaussSize, 1)
-
-    """
-    OPTI MULTITHREAD
-    """
-    # Gradient-X
-    grad_x = cv2.Sobel(gray,ddepth,1,0,ksize=3, scale = 1, delta = 0, borderType = cv2.BORDER_DEFAULT)
-    # Gradient-Y
-    grad_y = cv2.Sobel(gray,ddepth,0,1,ksize=3, scale = 1, delta = 0, borderType = cv2.BORDER_DEFAULT)
-
-    abs_grad_x = cv2.convertScaleAbs(grad_x)   # converting back to uint8
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-
-    dst = cv2.addWeighted(abs_grad_x,0.5,abs_grad_y,0.5,0)
-    ret,dst = cv2.threshold(dst,50,255,cv2.THRESH_BINARY)
-
     fps.update()
 
-    cv2.imshow(window_name, dst) 
+    cv2.imshow(window_name, frame) 
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q") :
     	break
